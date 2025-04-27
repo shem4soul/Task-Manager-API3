7012c28a4d919292cbb6f437dde6a98b05aef39c
shem4soul@gmail.com <shem4soul@gmail.com>
2025-04-27 17:44:01 +0100

    Add task management controller functions and update routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,33 @@
+const { get } = require("mongoose");
+
 const getAllTasks = (req, res) => {
   res.send('Get all items from the file' );
 }
 
+const createTask = (req, res) => {
+    res.send('Create a new task' );     
+    
+}
+
+const getTask = (req, res) => {
+    res.send('Get a single task' );     
+    
+}
+const updateTask = (req, res) => {
+    res.send('Update a task' );     
+    
+}
+const deleteTask = (req, res) => { 
+    res.send('Delete a task' );     
+    
+}
+
+
 module.exports = {
-  getAllTasks,
-  // add other controller functions here
+getAllTasks,
+ createTask,
+ getTask,
+ updateTask,
+deleteTask  
+
 }   
