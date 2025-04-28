1664e9ea6da538f5c5ea361f3a7f0a867bf95fb5
shem4soul@gmail.com <shem4soul@gmail.com>
2025-04-28 19:10:41 +0100

    Implement task creation functionality and add Task model schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,17 @@
-const { get } = require("mongoose");
+const Task = require('../models/tasks')
 
 const getAllTasks = (req, res) => {
   res.send('Get all items from the file' );
 }
 
-const createTask = (req, res) => {
-    res.json(req.body );     
-    
-}
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+    }
 
 const getTask = (req, res) => {
     res.json({id:req.params.id});     
