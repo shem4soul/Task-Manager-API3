e8d74b03bafa881a431ba656ae0037deb5986754
shem4soul@gmail.com <shem4soul@gmail.com>
2025-04-30 15:19:08 +0100

    Add custom error handling for task retrieval and deletion

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/tasks')
 const asyncWrapper = require('../middleware/async') 
+const {createCustomError} = require('../errors/custom-error')
 
 
 const getAllTasks = asyncWrapper(async(req, res) => {
@@ -13,30 +14,30 @@ const createTask = asyncWrapper(async (req, res) => {
     })
     
 
-const getTask = asyncWrapper( async (req, res) => {
+const getTask = asyncWrapper( async (req, res, next) => {
 
     const {id:taskID} = req.params
     const task = await Task.findOne({_id:taskID})
  if (!task) {
-    return res.status(404).json({msg: `No task with id : ${taskID}`})
-
+    return next(createCustomError(`No task with id : ${taskID}`, 404))  
+   
 }
     res.status(200).json({task})            
  } 
 )
 
-const deleteTask = asyncWrapper(async (req, res) => { 
+const deleteTask = asyncWrapper(async (req, res, next) => { 
 
     const {id:taskID} = req.params
     const task = await Task.findOneAndDelete({_id:taskID})
     if (!task) {
-        return res.status(404).json({msg: `No task with id : ${taskID}`})
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
     }
     res.status(200).json({task})
  })
 
 
-const updateTask = asyncWrapper( async (req, res) => {
+const updateTask = asyncWrapper( async (req, res, next) => {
 
     const {id : taskID } = req.params
 
@@ -46,7 +47,7 @@ const updateTask = asyncWrapper( async (req, res) => {
     })
 
     if (!task) {
-        return res.status(404).json({msg: `No task with id : ${taskID}`})
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
     }       
     
     res.status(200).json({task})
