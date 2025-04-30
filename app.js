86cc71444e05744bf6b2070db0e24359b25fdcb9
shem4soul@gmail.com <shem4soul@gmail.com>
2025-04-30 15:24:23 +0100

    Refactor app.js to improve middleware organization and error handling

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(notFound);
 // error handler middleware - this should be after your routes and notFound middleware
 app.use(errorHandlerMiddleware);
 
-const port = 3000;  
+const port = process.env.PORT || 3000;  
 
 const start = async () => { 
   try {
